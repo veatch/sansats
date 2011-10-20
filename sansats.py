5b4ce74378a225143a8774b9e953e720717cb4ba
Jason Veatch <jtveatch@gmail.com>
2011-10-19 21:18:14 -0400

    Added styling and timestamps to timeline.

--- sansats.py
+++ sansats.py
@@ -1,3 +1,4 @@
+import datetime
 import tweepy
 
 from flask import abort, Flask, render_template
@@ -33,6 +34,12 @@ def is_relevant_to_my_interests(tweet_text, following=None):
     elif following and tweet_text[1:].split() and tweet_text[1:].split()[0].lower() in following:
         return True
     return False
+
+def format_tweet_time(tweet_time):
+    # hard coding to eastern dst for now
+    return (tweet_time-datetime.timedelta(hours=4)).strftime('%B %d, %Y, %I:%M %p')
+
+app.jinja_env.filters['format_tweet_time'] = format_tweet_time
 
 if __name__ == '__main__':
     app.run(debug=True)
